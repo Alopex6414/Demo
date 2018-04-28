08ec7f3d25414b18e8d4281c0afe585672dd1a1a
Helium <1029535012@qq.com>
2018-04-28 15:36:44 +0800

    lua_demo v1.00a

---
+++ Release/script/demo.lua
@@ -0,0 +1,25 @@
+----------------------------------------------
+--File: demo.lua
+--Discript: test
+----------------------------------------------
+
+--variable
+var1 = 5
+var2 = 20
+
+--function
+function Add(a,b)
+    return a + b
+end
+
+function Sub(a,b)
+    return a - b
+end
+
+function Mul(a,b)
+    return a * b
+end
+
+function Dev(a,b)
+    return a / b
+end
